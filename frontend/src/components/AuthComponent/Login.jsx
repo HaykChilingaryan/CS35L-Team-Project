2e5de46353f0894daaae97b4ba6797b2806c68c8
Albert Mkhitaryan <albertmkhitaryan98@gmail.com>
2024-02-02 20:12:31 -0800

    Changed the font, removed forgot password, and removed some text.

--- frontend/src/components/AuthComponent/Login.jsx
+++ frontend/src/components/AuthComponent/Login.jsx
@@ -6,17 +6,14 @@ function Login() {
     <section className="vh-100">
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div className="col-12 col-md-8 col-lg-4 col-xl-5">
             <div
               className="card bg-dark text-white"
               style={{ borderRadius: "1rem" }}
             >
               <div className="card-body p-5 text-center">
-                <div className="mb-md-5 mt-md-4 pb-5">
-                  <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                  <p className="text-white-50 mb-5">
-                    Please enter your login and password!
-                  </p>
+                <div className="mb-md-5 mt-md-4 pb-1">
+                  <h2 className="fw-bold mb-2 text-uppercase pb-5">Login</h2>
                   <div className="form-outline form-white mb-4">
                     <input
                       type="text"
@@ -33,11 +30,6 @@ function Login() {
                       placeholder="Password"
                     />
                   </div>
-                  <p className="small mb-5 pb-lg-2">
-                    <a className="text-white-50" href="#!">
-                      Forgot password?
-                    </a>
-                  </p>
                   <button
                     className="btn btn-outline-light btn-lg px-5"
                     type="submit"

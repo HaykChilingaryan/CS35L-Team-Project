a60148ddb62d7a57d622a882c9ac95679eea51d7
Albert Mkhitaryan <albertmkhitaryan98@gmail.com>
2024-02-02 20:15:47 -0800

    had an extra import, just removed it.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import ""
 import Navigation from "./components/NavbarComponent/Navbar.jsx";
 import Login from "./components/AuthComponent/Login.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(

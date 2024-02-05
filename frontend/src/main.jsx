62e801b875959d1e74bc2d23a99764d8a121cc55
Albert Mkhitaryan <albertmkhitaryan98@gmail.com>
2024-02-04 16:46:37 -0800

    Also Added a Sidebar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import Navigation from "./components/NavbarComponent/Navbar.jsx";
 import Login from "./components/AuthComponent/Login.jsx";
+import Sidebar from "./components/SideBarComponent/Sidebar.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Navigation />
-    <Login />
+    {/* <Navigation />
+    <Login /> */}
+    <Sidebar />
   </React.StrictMode>
 );
